2d7891384e3d90e334459101bbbd9620168f7fb4
Dominique Feyer <dfeyer@ttree.ch>
2018-07-05 16:07:04 +0200

    TASK: Missing component from the previous commit

---
+++ src/components/form-input-wrapper/form-input-wrapper.tsx
@@ -0,0 +1,24 @@
+import {Component, Prop} from '@stencil/core';
+
+@Component({
+  tag: 'neos-form-input-wrapper',
+  styleUrl: 'styles/form-input-wrapper.dark.scss',
+  shadow: true
+})
+export class FormInputWrapper {
+
+  @Prop() label: string;
+
+  render() {
+    return (
+      <div class="label-wrapper">
+        <div class="label">
+          <neos-label label={this.label}></neos-label>
+        </div>
+        <div class="metadata">
+          <slot name="metadata"/>
+        </div>
+      </div>
+    );
+  }
+}
