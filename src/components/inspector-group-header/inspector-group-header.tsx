b3ae741988973509c311ea587853de91add6ab66
Dominique Feyer <dfeyer@ttree.ch>
2018-07-05 17:06:28 +0200

    TASK: Better hover on inspector group

--- src/components/inspector-group-header/inspector-group-header.tsx
+++ src/components/inspector-group-header/inspector-group-header.tsx
@@ -1,4 +1,4 @@
-import {Component, Prop, Event, EventEmitter} from '@stencil/core';
+import {Component, Prop, State, Event, EventEmitter} from '@stencil/core';
 
 @Component({
   tag: 'neos-inspector-group-header',
@@ -12,20 +12,38 @@ export class InspectorGroupHeader {
   @Prop() icon: string;
   @Prop() open: boolean = true;
 
+  @State() isHovered: boolean = false;
+
   neosVisibilitySwitchedHandlerHandler() {
     this.neosVisibilitySwitched.emit(!this.open);
+  }
+
+  mouseEnterHandler() {
+    this.isHovered = true;
+  }
+
+  mouseLeaveHandler() {
+    this.isHovered = false;
   }
 
   chevronIcon() {
     return this.open ? 'chevron-up' : 'chevron-down';
   }
 
+  buttonTheme() {
+    return this.isHovered ? 'brand' : 'transparent';
+  }
+
   render() {
-    return (<header>
+    return (<header
+      onMouseEnter={() => this.mouseEnterHandler()}
+      onmouseleave={() => this.mouseLeaveHandler()}
+      onClick={() => this.neosVisibilitySwitchedHandlerHandler()}
+    >
       <neos-icon squared name={this.icon} type="solid"></neos-icon>
       <neos-label theme="bold" label={this.label}></neos-label>
       <div class="toggle">
-        <neos-button squared theme="transparent" onClick={() => this.neosVisibilitySwitchedHandlerHandler()}>
+        <neos-button squared theme={this.buttonTheme()}>
           <neos-icon name={this.chevronIcon()} type="solid"></neos-icon>
         </neos-button>
       </div>
