1e284c116c577b71b502144c71432e2128cee521
Dominique Feyer <dfeyer@ttree.ch>
2018-07-06 11:03:49 +0200

    TASK: Add public method open/close for inspector group

--- src/components/inspector-group/inspector-group.tsx
+++ src/components/inspector-group/inspector-group.tsx
@@ -1,4 +1,4 @@
-import {Component, Prop, State, Listen} from '@stencil/core';
+import {Component, Event, EventEmitter, Method, Prop, State, Listen} from '@stencil/core';
 import classnames from 'classnames';
 
 @Component({
@@ -7,6 +7,9 @@ import classnames from 'classnames';
   shadow: true
 })
 export class InspectorGroup {
+  @Event() neosOpen: EventEmitter;
+  @Event() neosClose: EventEmitter;
+
   @Prop() label: string;
   @Prop() icon: string;
   @Prop() closed: boolean = false;
@@ -14,25 +17,47 @@ export class InspectorGroup {
   @State() isOpen : boolean;
   @State() isVisible : boolean;
 
+  neosOpenHandler() {
+    this.neosOpen.emit();
+  }
+
+  neosCloseHandler() {
+    this.neosClose.emit();
+  }
+
   @Listen('neosVisibilitySwitched')
   neosVisibilitySwitchedHandler(event: CustomEvent) {
     this.isOpen = event.detail;
-    this.queueVisibiltyChange();
+    this.handleVisibilityChange();
   }
 
   componentDidLoad() {
     this.isOpen = !this.closed;
-    this.queueVisibiltyChange();
+    this.handleVisibilityChange();
   }
 
-  queueVisibiltyChange() {
+  handleVisibilityChange() {
     if (this.isOpen) {
+      this.neosOpenHandler();
       setTimeout(() => {
         this.isVisible = true;
       }, 200);
     } else {
+      this.neosCloseHandler();
       this.isVisible = false;
     }
+  }
+
+  @Method()
+  close() {
+    this.isOpen = false;
+    this.handleVisibilityChange();
+  }
+
+  @Method()
+  open() {
+    this.isOpen = true;
+    this.handleVisibilityChange();
   }
 
   wrapperClassName() {
